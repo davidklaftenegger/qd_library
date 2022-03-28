b2d0fb8de8ce5f297989fcfdfd632cf4420a7f95
Kostis Sagonas <kostis@it.uu.se>
2022-03-28 22:39:26 +0200

    Upgrade to a newer version of GoogleTest and to CMake

--- .github/scripts/get_gtest.sh
+++ .github/scripts/get_gtest.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 
-VSN=1.7.0
+VSN=1.11.0
 
 cd tests
 wget https://github.com/google/googletest/archive/release-${VSN}.zip
 unzip release-${VSN}.zip
-mv googletest-release-${VSN} gtest-1.7.0
+mv googletest-release-${VSN} googletest
 cd ..
